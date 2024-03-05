063d8c8fd5e764a885a70fa28e6cfc5840bd053b
GeekTR <geektheripper@gmail.com>
2024-03-05 22:28:31 +0800

    update vaultwarden config

--- cloud-infra-vault/kubernetes/srv.vaultwarden.tf
+++ cloud-infra-vault/kubernetes/srv.vaultwarden.tf
@@ -8,6 +8,16 @@ module "vaultwarden_volume" {
   capacity         = "16Gi"
 }
 
+resource "random_password" "admin_token" {
+  length  = 48
+  special = false
+}
+
+resource "random_password" "vaultwarden_postgres_userpass" {
+  length  = 24
+  special = false
+}
+
 resource "kubernetes_deployment" "vaultwarden_deployment" {
   metadata {
     namespace = kubernetes_namespace.this.metadata.0.name
@@ -30,13 +40,56 @@ resource "kubernetes_deployment" "vaultwarden_deployment" {
         }
 
         container {
+          name  = "postgres"
+          image = "postgres:15-alpine"
+
+          env {
+            name  = "POSTGRES_DB"
+            value = "vaultwarden"
+          }
+
+          env {
+            name  = "POSTGRES_USER"
+            value = "vaultwarden"
+          }
+
+          env {
+            name  = "POSTGRES_PASSWORD"
+            value = random_password.vaultwarden_postgres_userpass.result
+          }
+
+          volume_mount {
+            name       = "vaultwarden-data-vol"
+            sub_path   = "postgres"
+            mount_path = "/var/lib/postgresql/data"
+          }
+        }
+
+        container {
           name  = "vaultwarden"
           image = "vaultwarden/server:latest"
           port { container_port = 80 }
 
           volume_mount {
             name       = "vaultwarden-data-vol"
+            sub_path   = "vaultwarden"
             mount_path = "/data"
+          }
+
+          dynamic "env" {
+            for_each = {
+              TZ                  = "Asia/Shanghai"
+              SIGNUPS_ALLOWED     = "false"
+              INVITATIONS_ALLOWED = "true"
+              SHOW_PASSWORD_HINT  = "false"
+              ADMIN_TOKEN         = random_password.admin_token.result
+              DOMAIN              = "https://bitwarden.geektr.co"
+              DATABASE_URL        = "postgres://vaultwarden:${random_password.vaultwarden_postgres_userpass.result}@localhost:5432/vaultwarden"
+            }
+            content {
+              name  = env.key
+              value = env.value
+            }
           }
         }
       }
